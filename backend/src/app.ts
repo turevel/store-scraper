29ee9365d1b2f85fec172f3bbd4b06a9df2eb2e1
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-28 14:50:31 -0300

    feat: create main app class

---
+++ backend/src/app.ts
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import express from 'express';
+
+class App {
+	public app: express.Express;
+
+	constructor() {
+		this.app = express();
+		this.config();
+		this.routes();
+	}
+
+	private config() {
+		this.app.use(express.json());
+		this.app.use(cors());
+	}
+
+	private routes() {
+		// Implementar
+	}
+
+	public start(port: number) {
+		this.app.listen(port, () => console.log(`App rodando na porta: ${port}...`));
+	}
+}
+
+export default App;
