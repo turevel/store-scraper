78d62478c068ad273c3d62f1f3aa6fd0ac402106
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 19:17:53 -0300

    feat: add products mock

---
+++ frontend/src/mocks/products.ts
@@ -0,0 +1,26 @@
+export default [
+	{
+		'image': 'https://i.zst.com.br/thumbs/45/17/34/-747048218.jpg',
+		'title': 'Smartphone Apple iPhone 13 128GB Câmera Dupla',
+		'link':
+      'https://www.buscape.com.br/celular/smartphone-apple-iphone-13-128gb-ios?_lc=11',
+		'price': 'R$ 4.268,99',
+		'company': 'Buscapé'
+	},
+	{
+		'image': 'https://i.zst.com.br/thumbs/45/2e/19/-789625783.jpg',
+		'title': 'Smartphone Apple iPhone 11 128GB Câmera Dupla',
+		'link':
+      'https://www.buscape.com.br/celular/smartphone-apple-iphone-11-128gb-ios?_lc=11',
+		'price': 'R$ 3.221,10',
+		'company': 'Buscapé'
+	},
+	{
+		'image': 'https://i.zst.com.br/thumbs/45/13/14/-795136300.jpg',
+		'title': 'Smartphone Samsung Galaxy S22 5G SM-S901 128GB Câmera Tripla',
+		'link': 'https://www.buscape.com.br/celular/smartphone-samsung-galaxy-'
+      + '128gb-android-camera-tripla-autofoco?_lc=11',
+		'price': 'R$ 3.098,00',
+		'company': 'Buscapé'
+	},
+];
