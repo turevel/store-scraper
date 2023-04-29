e56ad85d62356c2fadd8d40bb484317ea40a27bb
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 18:21:24 -0300

    feat: add animation on search area

--- frontend/src/components/search-area/index.tsx
+++ frontend/src/components/search-area/index.tsx
@@ -1,18 +1,28 @@
 import * as sc from './styles';
 
+import { RequestContext } from '../../provider/RequestProvider';
 import Search from '../search';
 
+import { useContext } from 'react';
 import { FaSpider } from 'react-icons/fa';
 
 function SearchArea() {
-	return (
-		<sc.Container>
-			<sc.Logo>
-				<FaSpider className="icon" />
-				Store Scraper
-			</sc.Logo>
+	const { loading } = useContext(RequestContext);
 
+	return (
+		<sc.Container
+			style={
+				{
+					height: loading ? '160px' : 'calc(50% - 20px)',
+				}
+			}
+		>
 			<sc.SearchContainer>
+				<sc.Logo>
+					<FaSpider className="icon" />
+				Store Scraper
+				</sc.Logo>
+
 				<Search />
 			</sc.SearchContainer>
 		</sc.Container>
--- frontend/src/components/search-area/styles/index.ts
+++ frontend/src/components/search-area/styles/index.ts
@@ -6,19 +6,25 @@ export const Container = styled.div`
   display: grid;
   place-items: center;
   position: relative;
-  height: 160px;
-`;
-
-export const Logo = styled.h1`
-  display: flex;
-  align-items: center;
-  column-gap: 10px;
-  font-size: 2.3rem;
+  transition: height ease-in-out 200ms;
 `;
 
 export const SearchContainer = styled.section`
+  align-items: center;
   bottom: 0;
+  display: flex;
+  flex-direction: column;
+  height: 125px;
+  justify-content: space-between;
   left: 50%;
   position: absolute;
-  transform: translate(-50%, 50%);
+  transform: translate(-50%, 20px);
 `;
+
+export const Logo = styled.h1`
+  align-items: center;
+  column-gap: 10px;
+  display: flex;
+  font-size: 2.3rem;
+`;
+
