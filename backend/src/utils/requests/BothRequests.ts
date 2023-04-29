2ef8e17fd1f3ce14746d3ebd6556722a10d600f5
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 13:45:39 -0300

    feat: create both requests

---
+++ backend/src/utils/requests/BothRequests.ts
@@ -0,0 +1,25 @@
+import BuscapeRequests from './BuscapeRequests';
+import MeliRequests from './MeliRequests';
+
+
+class BothRequests {
+	public static async getSmartphones() {
+		const meli = await MeliRequests.getSmartphones();
+		const buscape = await BuscapeRequests.getSmartphones();
+		return [...meli, ...buscape];
+	}
+
+	public static async getRefrigerators() {
+		const meli = await MeliRequests.getRefrigerators();
+		const buscape = await BuscapeRequests.getRefrigerators();
+		return [...meli, ...buscape];
+	}
+
+	public static async getTvs() {
+		const meli = await MeliRequests.getTvs();
+		const buscape = await BuscapeRequests.getTvs();
+		return [...meli, ...buscape];
+	}
+}
+
+export default BothRequests;
