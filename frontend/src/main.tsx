a99e27364181399769ef6985f41a4b84ca651613
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 16:14:43 -0300

    feat: starting front development

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './app.tsx';
+import './styles/reset.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from 'react-dom/client';
+
+ReactDOM
+	.createRoot(document.getElementById('root') as HTMLElement)
+	.render(<App />);
---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import * as sc from './styles';
+
+function Home() {
+	return (
+		<sc.Main>Página Principal</sc.Main>
+	);
+}
+
+export default Home;
