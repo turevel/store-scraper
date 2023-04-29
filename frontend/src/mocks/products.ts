c5ece3ffe87e240012bc682db339df401443dfa7
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 19:39:28 -0300

    feat: add more products to mock

--- frontend/src/mocks/products.ts
+++ frontend/src/mocks/products.ts
@@ -23,4 +23,32 @@ export default [
 		'price': 'R$ 3.098,00',
 		'company': 'Buscapé'
 	},
+	{
+		'image': 'https://http2.mlstatic.com/D_NQ_NP_627595-MLA49703413654_042022-V.jpg',
+		'title': 'Moto G22 Dual SIM 128 GB cosmic black 4 GB RAM',
+		'link': 'https://www.mercadolivre.com.br/moto-g22-dual-sim-128-gb-cosmic-black-'
+			+ '4-gb-ram/p/MLB19049539?pdp_filters=category:MLB1055#searchVariation='
+			+ 'MLB19049539&position=1&search_layout=stack&type=product&tracking_id='
+			+ 'f9ec957a-cea5-49c8-840a-efca2c5a7e4c',
+		'price': 'R$ 1.831,67',
+		'company': 'Mercado Livre'
+	},
+	{
+		'image': 'https://http2.mlstatic.com/D_NQ_NP_722576-MLB54759246403_032023-V.jpg',
+		'title': 'Smartphone Galaxy A54 5g 128gb 8gb Preto Samsung',
+		'link': 'https://produto.mercadolivre.com.br/MLB-3308558882-smartphone-galaxy-'
+			+ 'a54-5g-128gb-8gb-preto-samsung-_JM#position=34&search_layout=stack&type=i'
+			+ 'tem&tracking_id=f9ec957a-cea5-49c8-840a-efca2c5a7e4c',
+		'price': 'R$ 2.699',
+		'company': 'Mercado Livre'
+	},
+	{
+		'image': 'https://http2.mlstatic.com/D_NQ_NP_799905-MLB53198928114_012023-V.jpg',
+		'title': 'Smartphone Motorola Moto G32 128gb 4gb Ram 6,5 - Rose',
+		'link': 'https://produto.mercadolivre.com.br/MLB-3112429426-smartphone-motorola'
+			+ '-moto-g32-128gb-4gb-ram-65-rose-_JM#position=35&search_layout=stack&type='
+			+ 'item&tracking_id=f9ec957a-cea5-49c8-840a-efca2c5a7e4c',
+		'price': 'R$ 1.344,01',
+		'company': 'Mercado Livre'
+	},
 ];
