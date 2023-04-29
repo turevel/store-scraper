5e1e4289b637b9bf00db9f9b3fd5d09803e5cf86
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 15:56:51 -0300

    style: remove bank space

--- backend/src/classes/AbstractRequests.ts
+++ backend/src/classes/AbstractRequests.ts
@@ -5,7 +5,6 @@ import axios from 'axios';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
-
 export default abstract class AbstractRequests {
 	protected static readMock(filename: string) {
 		return readFileSync(resolve(__dirname, '..', 'mocks', filename));
