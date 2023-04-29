4d1071270d57f16a7efc78075c086552b719ab49
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 13:52:25 -0300

    feat: create index for direct import

---
+++ backend/src/abstract/index.ts
@@ -0,0 +1,4 @@
+export { default as AbstractController } from './AbstractController';
+export { default as AbstractService } from './AbstractService';
+export { default as AbstractRequests } from './AbstractRequests';
+export { default as AbstractExtractor } from './AbstractExtractor';
