6de385b9406782a46d56b4c41544b48cc1e99996
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 13:53:38 -0300

    feat: setting up route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -6,10 +6,11 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+router.get('/status', (_req: Request, res: Response) =>
+	res.status(200).json({ message: 'A API está em execução...' }));
+
 router.use('/smartphones', smartphonesRoute);
 router.use('/refrigerators', refrigeratorsRoute);
 router.use('/tvs', tvsRoute);
-router.get('/status', (_req: Request, res: Response) =>
-	res.status(200).json({ message: 'A API está em execução...' }));
 
 export default router;
--- backend/src/routes/smartphonesRoute.ts
+++ backend/src/routes/smartphonesRoute.ts
@@ -1,5 +1,9 @@
+import SmartphonesController from '../controllers/SmartphonesController';
+
 import express from 'express';
 
 const router = express.Router();
+
+router.get('/', SmartphonesController.get);
 
 export default router;
