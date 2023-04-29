59911e8add5ed345cd6bc17705e7ba55746dbdda
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-29 19:46:31 -0300

    fix: width

--- frontend/src/pages/home/styles/index.ts
+++ frontend/src/pages/home/styles/index.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   background-color: #fafafa;
-  height: max(100vh, 620px);
-  width: 1000px;
+  min-height: max(100vh, 620px);
   margin-inline: auto;
   box-shadow: 0 0 10px rgba(0 0 0 / 12%);
   position: relative;
