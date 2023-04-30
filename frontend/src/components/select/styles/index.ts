ed69ef86a1ca266b901e007241eec9e4dfc9feb8
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 17:11:14 -0300

    feat: add hover background and adjust height

--- frontend/src/components/select/styles/index.ts
+++ frontend/src/components/select/styles/index.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Select = styled.select`
-  height: 28px;
+  height: 30px;
   padding-inline: 5px;
   border-radius: 2px;
   background-color: ${({ theme }) => theme.colors.accentSecondary};
@@ -9,6 +9,10 @@ export const Select = styled.select`
   cursor: pointer;
   width: 120px;
   box-shadow: 0 0 5px rgba(0 0 0 / 12%);
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.accentTertiary};
+  }
 `;
 
 export const Option = styled.option`
