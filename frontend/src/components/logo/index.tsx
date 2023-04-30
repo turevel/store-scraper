b4b80275b17714e8fa008badd9f89b2d13987b3c
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 17:03:16 -0300

    fix: logo styles

---
+++ frontend/src/components/logo/index.tsx
@@ -0,0 +1,31 @@
+import * as sc from './styles';
+
+import { RequestContext } from '../../provider/RequestProvider';
+
+import { useContext } from 'react';
+import { FaSpider } from 'react-icons/fa';
+
+const INITIAL_STYLE = {
+	fontSize: '2.3rem',
+	top: 'calc(50vh - 180px)',
+	left: '50%',
+	transform: 'translateX(-50%)'
+};
+
+const MOVE_STYLE = {
+	fontSize: '1.7rem',
+	top: '15px',
+	left: '15px',
+};
+
+function Logo() {
+	const { loading, data } = useContext(RequestContext);
+	return (
+		<sc.Logo style={ loading || data.length > 0 ? MOVE_STYLE : INITIAL_STYLE }>
+			<FaSpider className="icon" />
+			Store Scraper
+		</sc.Logo>
+	);
+}
+
+export default Logo;
---
+++ frontend/src/components/logo/styles/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Logo = styled.h1`
+  align-items: center;
+  column-gap: 10px;
+  display: flex;
+  font-size: 1.7rem;
+  top: 15px;
+  left: 15px;
+  position: absolute;
+  transition: all ease-in-out 200ms;
+`;
