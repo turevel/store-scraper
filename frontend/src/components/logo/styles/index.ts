6480a53b9e86a8af280fabb66241490ea344c36c
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 17:09:43 -0300

    feat: add text shadow to logo

--- frontend/src/components/logo/styles/index.ts
+++ frontend/src/components/logo/styles/index.ts
@@ -4,9 +4,7 @@ export const Logo = styled.h1`
   align-items: center;
   column-gap: 10px;
   display: flex;
-  font-size: 1.7rem;
-  top: 15px;
-  left: 15px;
   position: absolute;
   transition: all ease-in-out 200ms;
+  text-shadow: 0 0 5px rgba(0 0 0 / 12%);
 `;
