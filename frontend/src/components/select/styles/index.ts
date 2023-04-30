51432a5b56fec260f0d3335b543e08a9eb4f94df
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 16:10:43 -0300

    feat: add select component

---
+++ frontend/src/components/select/styles/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Select = styled.select`
+  height: 28px;
+  padding-inline: 5px;
+  border-radius: 2px;
+  background-color: ${({ theme }) => theme.colors.accentSecondary};
+  color: ${({ theme }) => theme.colors.fontLight};
+  cursor: pointer;
+  width: 120px;
+  box-shadow: 0 0 5px rgba(0 0 0 / 12%);
+`;
+
+export const Option = styled.option`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.font};
+`;
