c64c87639103035ee95f0c9adbd250ccd36fee41
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 20:32:15 -0300

    feat: add provider to app

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,18 @@
 import Home from './pages/home';
 
+import { AppContext } from './provider/AppProvider';
 import RequestProvider from './provider/RequestProvider';
 import * as themes from './styles/themes';
 
+import { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 function App() {
+	const { theme } = useContext(AppContext);
+
 	return (
 		<RequestProvider>
-			<ThemeProvider theme={ themes.light }>
+			<ThemeProvider theme={ themes[theme] }>
 				<Home  />
 			</ThemeProvider>
 		</RequestProvider>
