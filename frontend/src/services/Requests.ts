2a0a45ffdbbcd72b1e21d7e7d6a82a8f8d73c309
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 18:43:23 -0300

    feat: create requests class

---
+++ frontend/src/services/Requests.ts
@@ -0,0 +1,22 @@
+import IProducts from '../interfaces/IProducts';
+import Categories from '../types/Categories';
+import Companies from '../types/Companies';
+
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001';
+
+class Requests {
+	private static ax = axios.create({ baseURL: API_URL });
+
+	public static async getCategory(category: Categories, company: Companies):
+    Promise<IProducts[]> {
+		try {
+			const url = `${API_URL}/${category}?company=${company}`;
+			const { data } = await Requests.ax.get(url);
+			return data;
+		} catch (_) { return []; }
+	}
+}
+
+export default Requests;
