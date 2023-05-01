1a074edfc200e5fc576cf9809fac4d9eeafd797e
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-05-01 17:05:38 -0300

    feat: add model actions on service

--- backend/src/services/SearchService.ts
+++ backend/src/services/SearchService.ts
@@ -1,16 +1,35 @@
+import SearchModel from '../db/models/SearchModel';
 import IProduct from '../interfaces/IProducts';
 import Companies from '../types/Companies';
 import { BothRequests, BuscapeRequests, MeliRequests } from '../utils/requests';
 
 class SearchService  {
 	public async search(query: string, company: Companies): Promise<IProduct[]> {
+		const queryAndCompany = `${query}&company=${company}`;
+
+		const findProducts = await SearchModel
+			.findOne({ query: queryAndCompany });
+
+		if (findProducts) {
+			return findProducts.products
+				.map(({ image, title, link, price, company }) =>
+					({image, title, link, price, company }));
+		}
+
 		const requests = {
 			both: BothRequests.search,
 			meli: MeliRequests.search,
 			buscape: BuscapeRequests.search,
 		};
 
-		return requests[company](query);
+		const data = await requests[company](query);
+
+		await SearchModel.create({
+			query: queryAndCompany,
+			products: data,
+		});
+
+		return data;
 	}
 }
 
