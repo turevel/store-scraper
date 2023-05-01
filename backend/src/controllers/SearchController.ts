68537d5c08b898c27b6be1efb797e60670d0fb3d
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-04-30 23:47:50 -0300

    feat: create search controller

---
+++ backend/src/controllers/SearchController.ts
@@ -0,0 +1,29 @@
+import { SearchService } from '../services';
+import Companies from '../types/Companies';
+
+import { Request, Response } from 'express';
+
+class SearchController {
+	private companies: Companies[] = ['both', 'meli', 'buscape'];
+	private service: SearchService;
+
+	constructor() {
+		this.service = new SearchService();
+		this.search = this.search.bind(this);
+	}
+
+	private getCompanyName(company: string): Companies {
+		if (!this.companies.includes(company as Companies)) return 'both';
+		return company as Companies;
+	}
+
+	public async search({ query }: Request, res: Response) {
+		const search = String(query.q);
+		const company = this.getCompanyName(String(query.company));
+		console.log(search);
+		console.log(company);
+		return res.status(200).json((await this.service.search(search, company)));
+	}
+}
+
+export default SearchController;
