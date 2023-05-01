4efc5e5980f0de65d305eec8c35b1cd8a5441529
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-05-01 15:57:06 -0300

    feat: add connection before start server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import App from './app';
-
-import dotenvSafe from 'dotenv-safe';
-
-dotenvSafe.config();
+import connection from './db/connection';
 
 const API_PORT = Number(process.env.API_PORT);
 
-new App().start(API_PORT);
+connection()
+	.then(() => new App().start(API_PORT))
+	.catch(() => console.log('Falha ao se conectar com o banco de dados!'));
