4b5837fe3999499c87193f14701297127753ca8b
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-05-01 00:54:52 -0300

    feat: add request event and button state

--- frontend/src/components/search/index.tsx
+++ frontend/src/components/search/index.tsx
@@ -1,16 +1,30 @@
 import * as sc from './styles';
 
+import { RequestContext } from '../../provider/RequestProvider';
+
+import { useContext } from 'react';
 import { BiSearch } from 'react-icons/bi';
 
 function Search() {
+	const {
+		loading,
+		search,
+		changeSearch,
+		requestSearch,
+	} = useContext(RequestContext);
+
 	return (
 		<sc.Container>
 			<sc.Input
+				onChange={ (e) => changeSearch(e.target.value) }
 				placeholder="Digite um termo de busca..."
 				type="input"
+				value={ search }
 			/>
 
 			<sc.Submit
+				disabled={ loading }
+				onClick={ requestSearch }
 				title="Pesquisar"
 				type="button"
 			>
