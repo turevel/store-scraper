94a183842a89f9757ed26b0d0a74068ce67bc6b3
Leonardo Araújo <ogbeni.leonardo@outlookcom>
2023-05-04 14:10:23 -0300

    feat: implements new logic to theme switcher

--- frontend/src/components/theme-switcher/styles/index.ts
+++ frontend/src/components/theme-switcher/styles/index.ts
@@ -16,76 +16,63 @@ export const Container = styled.div`
   transition: transform ease-in-out 100ms;
   z-index: 9999;
 
-  &:has(#switch-expand:checked) {
+  &[data-expand=true] {
     transform: translateX(0);
   }
 `;
 
-export const SettingsLabel = styled.label`
+export const Settings = styled.button`
+  background-color: transparent;
   border-radius: 50%;
-  cursor: pointer;
   color: ${({ theme }) => theme.colors.font};
+  cursor: pointer;
   display: grid;
   height: 35px;
   place-items: center;
   position: relative;
   width: 35px;
 
-  & > input {
-    cursor: pointer;
-    opacity: 0;
-    position: absolute;
-  }
-
   &:hover {
     background-color: ${({ theme }) => theme.colors.secondary};
   }
 `;
 
-export const ThemesContainer = styled.div`
-  & label {
-    align-items: center;
-    background-color: ${({ theme }) => theme.colors.secondary};
-    color: ${({ theme }) => theme.colors.font};
-    border-radius: 4px;
-    column-gap: 5px;
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    padding: 6px;
+export const Switcher = styled.button`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.font};
+  border-radius: 4px;
+  column-gap: 5px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  padding: 6px;
 
-    & > input {
-      cursor: pointer;
-      opacity: 0;
-      position: absolute;
-    }
+  &[data-lights-on=true] > span {
+    background-color: ${({ theme }) => theme.colors.accentSecondary};
 
-    &:has(input:checked) > .slider {
-      background-color: ${({ theme }) => theme.colors.accentSecondary};
-
-      & > .slider-thumb {
-        transform: translateX(100%);
-      }
+    & > span {
+      transform: translateX(100%);
     }
   }
+`;
 
-  & .slider {
-    align-items: center;
-    background-color: ${({ theme }) => theme.colors.tertiary};
-    border-radius: 18px;
-    width: 38px;
-    display: flex;
-    height: 20px;
-    padding-inline: 1px;
-    cursor: pointer;
-  }
+export const Slider = styled.span`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.tertiary};
+  border-radius: 18px;
+  cursor: pointer;
+  display: flex;
+  height: 20px;
+  padding-inline: 1px;
+  width: 38px;
+`;
 
-  & .slider-thumb {
-    background-color: ${({ theme }) => theme.colors.fontLight};
-    border-radius: 50%;
-    cursor: pointer;
-    height: 18px;
-    transition: transform ease-in-out 100ms;
-    width: 18px;
-  }
+export const SliderThumb = styled.span`
+  background-color: ${({ theme }) => theme.colors.fontLight};
+  border-radius: 50%;
+  cursor: pointer;
+  height: 18px;
+  transition: transform ease-in-out 100ms;
+  width: 18px;
 `;
